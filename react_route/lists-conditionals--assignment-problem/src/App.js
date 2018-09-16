2b9b9a6e16f6e77ddb51f0812eb66697864e1339
David Lee <DavidLee8two7@gmail.com>
2018-09-15 18:57:46 -0700

    working with lists and conditionals

---
+++ react_route/lists-conditionals--assignment-problem/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Validation from "./validation/Validation";
+import Char from "./char/Char";
+
+class App extends Component {
+  state = {
+    userInput: ""
+  };
+
+  onChangeHandler = event => {
+    this.setState({ userInput: event.target.value });
+  };
+
+  deleteCharHandler = index => {
+    const text = this.state.userInput.split("");
+    text.splice(index, 1);
+    const updatedText = text.join("");
+    this.setState({ userInput: updatedText });
+  };
+
+  render() {
+    const charlist = this.state.userInput.split("").map((ch, index) => {
+      return (
+        <Char
+          character={ch}
+          key={index}
+          clicked={() => this.deleteCharHandler(index)}
+        />
+      );
+    });
+
+    return (
+      <div className="App">
+        <input
+          type="text"
+          onChange={this.onChangeHandler}
+          value={this.state.userInput}
+        />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charlist}
+      </div>
+    );
+  }
+}
+
+export default App;
