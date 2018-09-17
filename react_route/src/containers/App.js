0690c77437b61b4c453dbb79f7db36fe712082c0
David Lee <DavidLee8two7@gmail.com>
2018-09-16 22:46:51 -0700

    re-structure components

--- react_route/src/containers/App.js
+++ react_route/src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import classes from "./App.css";
-import Person from "./Person/Person";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+import Person from "../components/Persons/Person/Person";
 
 class App extends Component {
   state = {
@@ -49,14 +48,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
-                <Person
-                  click={() => this.deleteHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  changed={event => this.changeHandler(event, person.id)}
-                />
-              </ErrorBoundary>
+              <Person
+                click={() => this.deleteHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.changeHandler(event, person.id)}
+              />
             );
           })}
         </div>
