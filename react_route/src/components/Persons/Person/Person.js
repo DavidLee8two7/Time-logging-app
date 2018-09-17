73735a81cbd9ee388e4870e1f56b586c422d3292
David Lee <DavidLee8two7@gmail.com>
2018-09-17 15:22:17 -0700

    Dont have to use unnecessary div by using higher over function with <Aux />

--- react_route/src/components/Persons/Person/Person.js
+++ react_route/src/components/Persons/Person/Person.js
@@ -17,19 +17,33 @@ class Person extends Component {
 
   render() {
     console.log("[Person.js] Inside render()");
-    return (
-      <div className={classes.person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old.
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          placeholder={this.props.name}
-        />
-      </div>
-    );
+
+    // return (
+    //   <div className={classes.person}>
+    //     <p onClick={this.props.click}>
+    //       I'm {this.props.name} and I am {this.props.age} years old.
+    //     </p>
+    //     <p>{this.props.children}</p>
+    //     <input
+    //       type="text"
+    //       onChange={this.props.changed}
+    //       value={this.props.name}
+    //     />
+    //   </div>
+    // );
+
+    return [
+      <p key="hio" onClick={this.props.click}>
+        I'm {this.props.name} and I am {this.props.age} years old.
+      </p>,
+      <p key="hiwefo">{this.props.children}</p>,
+      <input
+        key="hwefwio"
+        type="text"
+        onChange={this.props.changed}
+        value={this.props.name}
+      />
+    ];
   }
 }
 
