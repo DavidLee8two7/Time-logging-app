3cf0f88b9815323af44e2243db1a8f7b9227681b
David Lee <DavidLee8two7@gmail.com>
2018-09-17 17:11:02 -0700

    hoc using function component to reate class component

--- react_route/src/hoc/WithClass.js
+++ react_route/src/hoc/WithClass.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
+// Using it as div with class
 // const withClass = props => {
 //   return <div className={props.classes}>{props.children}</div>;
 // };
 
+// Using it as function to create component
+// const withClass = (WrappedComponent, className) => {
+//   return props => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />
+//     </div>
+//   );
+// };
+
+// Using it as function to create component
 const withClass = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent {...props} />
-    </div>
-  );
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
 };
 
 export default withClass;
