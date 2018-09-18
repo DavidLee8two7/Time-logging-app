e6b7a15034256b08e5e266cd9252ae8f37d17534
David Lee <DavidLee8two7@gmail.com>
2018-09-17 17:00:09 -0700

    higher order component using spread operator

--- react_route/src/components/Persons/Person/Person.js
+++ react_route/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./person.css";
-import WithClass from "../../../hoc/WithClass";
+import Aux from "../../../hoc/Aux";
+import withClass from "../../../hoc/WithClass";
 
 class Person extends Component {
   constructor(props) {
@@ -20,7 +21,7 @@ class Person extends Component {
     console.log("[Person.js] Inside render()");
 
     return (
-      <WithClass classes={classes.person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old.
         </p>
@@ -30,9 +31,9 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default Person;
+export default withClass(Person, classes.person);
--- react_route/src/containers/App.js
+++ react_route/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
-import WithClass from "../hoc/WithClass";
+import Aux from "../hoc/Aux";
+import withClass from "../hoc/WithClass"; // returning a function now
 
 class App extends PureComponent {
   constructor(props) {
@@ -94,7 +95,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showPersons: true });
@@ -109,9 +110,9 @@ class App extends PureComponent {
           clicked={this.toggleHandler}
         />
         {people}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
